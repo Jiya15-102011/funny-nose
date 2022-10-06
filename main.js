c876cfaa7b53b6d85d0f4e28b3537cdfb0d6817d
Jiya15-102011 <98893406+Jiya15-102011@users.noreply.github.com>
2022-10-06 19:35:50 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+nosex=0;
+nosey=0;
+function preload(){
+    b=loadImage('https://i.postimg.cc/7ZBcjDqp/clownnose.png');
+}
+function setup(){
+    canvas=createCanvas(300,300);
+    canvas.center();
+    d=createCapture(VIDEO);
+    d.size(300,300);
+    d.hide();
+    y=ml5.poseNet(d,loaded);
+    y.on('pose',poseparts);
+}
+function loaded(){
+console.log("poseNet is intilized");
+}
+function poseparts(answer){
+if(answer.length>0){
+console.log(answer);
+nosex=answer[0].pose.nose.x-15;
+nosey=answer[0].pose.nose.y-15;
+}
+}
+function draw(){
+    image(d,0,0,300,300)
+    image(b,nosex,nosey,30,30);
+
+}
+function a(){
+    save('clown_nose.jpeg');
+}
